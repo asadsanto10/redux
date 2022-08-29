3ecbc39253760861d672f372f7d11dfec275e8ba
santo.meridian@gmail.com <santo.meridian@gmail.com>
2022-08-29 21:31:42 +0600

    redux-video-project

--- src/App.js
+++ src/App.js
@@ -1,38 +1,58 @@
 import React from 'react';
-import { Provider } from 'react-redux';
-import CompletedTodoList from './components/CompletedTodoList';
-import Footer from './components/Footer';
-import Header from './components/Header';
-import Navbar from './components/Navbar';
-import TodoList from './components/TodoList';
-import store from './redux/store';
+import './App.css';
+import { Counter } from './features/counter/Counter';
+import logo from './logo.svg';
 
-const App = () => {
+function App() {
   return (
-    <Provider store={store}>
-      <div className="App">
-        <div className="grid place-items-center bg-blue-100 px-6 font-sans h-screen">
-          <Navbar />
-
-          <div className="w-full max-w-3xl shadow-lg rounded-lg p-6 bg-white">
-            <Header />
-            <hr className="mt-4" />
-
-            <TodoList />
-
-            <hr className="mt-4" />
-
-            <Footer />
-          </div>
-
-          <div className="w-full max-w-3xl shadow-lg rounded-lg p-6 bg-white mt-4">
-            <h2>Completed Todo</h2>
-            <CompletedTodoList />
-          </div>
-        </div>
-      </div>
-    </Provider>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <Counter />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <span>
+          <span>Learn </span>
+          <a
+            className="App-link"
+            href="https://reactjs.org/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            React
+          </a>
+          <span>, </span>
+          <a
+            className="App-link"
+            href="https://redux.js.org/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Redux
+          </a>
+          <span>, </span>
+          <a
+            className="App-link"
+            href="https://redux-toolkit.js.org/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Redux Toolkit
+          </a>
+          ,<span> and </span>
+          <a
+            className="App-link"
+            href="https://react-redux.js.org/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            React Redux
+          </a>
+        </span>
+      </header>
+    </div>
   );
-};
+}
 
 export default App;
--- src/redux/todos/actionType.js
+++
@@ -1,8 +0,0 @@
-export const LOADED = 'todos/loded';
-export const ADDED = 'todos/added';
-export const UPDATED = 'todos/update';
-export const TOGGLED = 'todos/toggled';
-export const COLORSELECTED = 'todos/colorselected';
-export const DELETED = 'todos/deleted';
-export const ALLCOMPLETED = 'todos/allcompleted';
-export const CLEARCOMPLETED = 'todos/clearcompleted';
