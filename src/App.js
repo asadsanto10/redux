93bee61941364745ccc493d9347a9651248eb113
santo.meridian@gmail.com <santo.meridian@gmail.com>
2022-09-04 00:27:15 +0600

    redux advance rtk CURD app

--- src/App.js
+++ src/App.js
@@ -1,18 +1,9 @@
 import React from 'react';
-import { Route, Routes } from 'react-router-dom';
-import Navbar from './components/navbar/Navbar';
-import Home from './pages/Home';
-import Video from './pages/Video';
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/videos/:videoId" element={<Video />} />
-      </Routes>
-      <footer />
+      <h1>Hello</h1>
     </div>
   );
 }
