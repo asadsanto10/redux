5a3245c6abb8e527add3a4f4d801653834eaf466
santo.meridian@gmail.com <santo.meridian@gmail.com>
2022-09-12 00:33:19 +0600

    advance project chat-app

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,17 @@
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import { apiSlice } from '../features/api/apiSlice';
+import authSlice from '../features/auth/authSlice';
+import conversationsSlice from '../features/conversations/conversationsSlice';
+
+import messagesSlice from '../features/messages/messagesSlice';
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
+    [apiSlice.reducer]: apiSlice.reducer,
+    auth: authSlice.reducer,
+    conversations: conversationsSlice.reducer,
+    messages: messagesSlice.reducer,
   },
+  devTools: process.env.NODE_ENV !== 'production',
+  middleware: (getDefaultMiddlewares) => getDefaultMiddlewares().concat(apiSlice.middleware),
 });
