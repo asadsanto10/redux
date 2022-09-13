9342f81c95692cbb1165da76a5a5a68b5215c75e
asadsanto10 <santo.meridian@gmail.com>
2022-09-13 18:09:52 +0600

    update

--- src/components/inbox/chatbody/ChatBody.js
+++ src/components/inbox/chatbody/ChatBody.js
@@ -1,18 +1,42 @@
 // import Blank from "./Blank";
+import { useParams } from 'react-router-dom';
+import { useGetMessagesQuery } from '../../../features/messages/messagesApi';
+import Error from '../../ui/Error';
 import ChatHead from './ChatHead';
 import Messages from './Messages';
 import Options from './Options';
 
 export default function ChatBody() {
-  return (
-    <div className="w-full lg:col-span-2 lg:block">
-      <div className="w-full grid conversation-row-grid">
+  const { id } = useParams();
+
+  const { data, isError, isLoading, error } = useGetMessagesQuery(id);
+
+  // console.log(data);
+
+  // render content
+  let content = null;
+
+  if (isLoading) content = <div>Loading...</div>;
+
+  if (!isLoading && isError) content = <Error message={error} />;
+  if (!isLoading && !isError && data?.length === 0) content = <div>No Messages yet</div>;
+
+  if (!isLoading && !isError && data?.length > 0)
+    content = (
+      <>
         <ChatHead
           avatar="https://cdn.pixabay.com/photo/2018/01/15/07/51/woman-3083383__340.jpg"
           name="Akash Ahmed"
         />
-        <Messages />
+        <Messages messages={data} />
         <Options />
+      </>
+    );
+
+  return (
+    <div className="w-full lg:col-span-2 lg:block">
+      <div className="w-full grid conversation-row-grid">
+        {content}
         {/* <Blank /> */}
       </div>
     </div>
--- src/components/inbox/chatbody/Messages.js
+++ src/components/inbox/chatbody/Messages.js
@@ -1,27 +1,28 @@
+import { useSelector } from 'react-redux';
 import Message from './Message';
 
-export default function Messages() {
+export default function Messages({ messages = [] }) {
+  console.log(messages);
+
+  const {
+    user: { email },
+  } = useSelector((state) => state.auth) || {};
+
   return (
     <div className="relative w-full h-[calc(100vh_-_197px)] p-6 overflow-y-auto flex flex-col-reverse">
       <ul className="space-y-2">
-        <Message justify="start" message="Hjdjd" />
-        <Message justify="start" message="How are you?" />
-        <Message justify="end" message="I am fine what about you?" />
-        <Message justify="start" message="Hi" />
-        <Message justify="start" message="How are you?" />
-        <Message justify="end" message="I am fine what about you?" />
-        <Message justify="start" message="Hi" />
-        <Message justify="start" message="How are you?" />
-        <Message justify="end" message="I am fine what about you?" />
-        <Message justify="start" message="Hi" />
-        <Message justify="start" message="How are you?" />
-        <Message justify="end" message="I am fine what about you?" />
-        <Message justify="start" message="Hi" />
-        <Message justify="start" message="How are you?" />
-        <Message justify="end" message="I am fine what about you?" />
-        <Message justify="start" message="Hi" />
-        <Message justify="start" message="How are you?" />
-        <Message justify="end" message="I am fine what about you?" />
+        {messages
+          .slice()
+          .sort((a, b) => a.timestamp - b.timestamp)
+          .map(({ id, message, sender }) => {
+            return (
+              <Message
+                key={id}
+                justify={sender.email !== email ? 'start' : 'end'}
+                message={message}
+              />
+            );
+          })}
       </ul>
     </div>
   );
